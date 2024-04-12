7b091b8183bf3f32a1c4e89e10bb0508ee22e0f5
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-04-12 12:58:50 +0530

    get message remaining

--- src/components/home/chat-bubble.tsx
+++ src/components/home/chat-bubble.tsx
@@ -1,4 +1,11 @@
-const ChatBubble = () => {
+import { IMessage } from "@/store/chat-store";
+
+
+type ChatBubbleProps = {
+	message: IMessage;
+	me: any;
+};
+const ChatBubble = ({me, message}: ChatBubbleProps) => {
 	return <div>ChatBubble</div>;
 };
 export default ChatBubble;
