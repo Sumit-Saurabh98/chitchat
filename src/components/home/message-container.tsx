bcdeb9ffd3a3ea48f536bbaca654e5ab0bdb9e8b
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-05-06 10:02:57 +0530

    get message added

--- src/components/home/message-container.tsx
+++ src/components/home/message-container.tsx
@@ -1,30 +1,21 @@
-
 import ChatBubble from "./chat-bubble";
-import { useConversationStore } from "@/store/chat-store";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
-import { useEffect, useRef } from "react";
+import { useConversationStore } from "@/store/chat-store";
 
 const MessageContainer = () => {
-
 	const { selectedConversation } = useConversationStore();
 	const messages = useQuery(api.messages.getMessages, {
 		conversation: selectedConversation!._id,
 	});
 	const me = useQuery(api.users.getMe);
-	// const lastMessageRef = useRef<HTMLDivElement>(null);
 
-	// useEffect(() => {
-	// 	setTimeout(() => {
-	// 		lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
-	// 	}, 100);
-	// }, [messages]);
 	return (
 		<div className='relative p-3 flex-1 overflow-auto h-full bg-chat-tile-light dark:bg-chat-tile-dark'>
 			<div className='mx-12 flex flex-col gap-3 h-full'>
 				{messages?.map((msg, idx) => (
 					<div key={msg._id}>
-						<ChatBubble message={msg} me={me}  />
+						<ChatBubble message={msg} me={me} previousMessage={idx > 0 ? messages[idx - 1] : undefined} />
 					</div>
 				))}
 			</div>
